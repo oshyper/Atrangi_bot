6d4697cd4e13accb992700062f352cdae2fd1706
oshyper <oshyper442@gmail.com>
2026-09-17 11:25:22 +0530

    Delete main.py

--- app/main.py
+++
@@ -1,8 +0,0 @@
-import asyncio, os
-import uvicorn
-from app.bot import main as bot_main
-async def web_main():
-    config=uvicorn.Config('app.web:app',host='0.0.0.0',port=int(os.getenv('PORT','8000')),log_level='info')
-    await uvicorn.Server(config).serve()
-async def main(): await asyncio.gather(bot_main(),web_main())
-if __name__=='__main__': asyncio.run(main())
